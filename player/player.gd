dda81384cf13648bbb850f9233ac94e7cc47de9a
Jeronimo Schreyer <jeronimo.schreyer@gmail.com>
2025-11-27 14:20:41 -0300

    fixed idle player state

--- player/player.gd
+++ player/player.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 
 @onready var _viewport = get_viewport()
 
+var _last_direction := Vector2.RIGHT
 var _last_focused_object
 var locked := false
 
@@ -13,8 +14,7 @@ var locked := false
 func _physics_process(_delta: float) -> void:
 	if locked: return
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction and handle the movement
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if direction:
 		velocity = direction * SPEED
@@ -23,11 +23,20 @@ func _physics_process(_delta: float) -> void:
 
 	move_and_slide()
 
-	# align raycast
-	if velocity.length() > 0.1:
-		$RayCast2D.global_position = global_position
-		$RayCast2D.target_position = velocity * RAYCAST_SCALE
+	# update last input direction
+	if direction.length() > 0.1:
+		_last_direction = direction
 
+
+func _process(_delta: float) -> void:
+	# update animation tree
+	$AnimationTree.set("parameters/Idle/blend_position", _last_direction)
+	$AnimationTree.set("parameters/Moving/blend_position", _last_direction)
+
+	# align raycast
+	$RayCast2D.target_position = _last_direction * RAYCAST_SCALE
+
+	# check for interactable collisions
 	if $RayCast2D.is_colliding():
 		var collider : Object = $RayCast2D.get_collider().get_parent()
 		if collider.has_method("focus"):
@@ -36,9 +45,6 @@ func _physics_process(_delta: float) -> void:
 			update_focus(null)
 	else:
 		update_focus(null)
-
-	$AnimationTree.set("parameters/blend_position", velocity)
-	$AnimationTree.active = not velocity.is_equal_approx(Vector2.ZERO)
 
 
 func update_focus(new_focus):
