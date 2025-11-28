cc0dca739faab3a2fc023bd14006d9c09c0b08e0
Jeronimo Schreyer <jeronimo.schreyer@gmail.com>
2025-11-28 15:59:01 -0300

    commiteo todo

--- interactables/item.gd
+++ interactables/item.gd
@@ -3,7 +3,14 @@ extends Area2D
 @onready var _animation_player: AnimationPlayer = $AnimationPlayer
 
 @export var _item_sprite: Texture
+@export var collision : Shape2D :
+	set(v):
+		collision = v
+		if has_node("CollisionShape2D"):
+			$CollisionShape2D.shape = v
+
 @export_enum("mate", "medialuna", "empanada")
+
 var _name_item: String
 
 func _ready() -> void:
@@ -11,7 +18,7 @@ func _ready() -> void:
 
 func _changue_item() -> void:
 	$Sprite2D.texture = _item_sprite
-	
+
 func _verify_collection(name_item: String) -> void:
 	match name_item:
 		"mate":
@@ -32,3 +39,9 @@ func _on_body_entered(body: CharacterBody2D) -> void:
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	queue_free()
+
+
+func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	if area.get_parent() is Player:
+		_verify_collection(_name_item)
+		_animation_player.play("fade_out")
