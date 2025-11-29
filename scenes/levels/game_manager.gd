a1d24a49e5e00f55d90956b4fc69acbab0b347e6
Jeronimo Schreyer <jeronimo.schreyer@gmail.com>
2025-11-29 04:21:16 -0300

    added intro narrative

--- scenes/levels/game_manager.gd
+++ scenes/levels/game_manager.gd
@@ -4,4 +4,18 @@ var medialuna_count: int = 0
 var empanada_count: int = 0
 
 
-	
+func _ready() -> void:
+	get_tree().scene_changed.connect(_on_scene_changed)
+
+
+func _on_scene_changed():
+	if get_tree().has_group("player"):
+		var player = get_tree().get_nodes_in_group("player")[0]
+		player.set_locked(true)
+		var balloon = DialogueManager.show_dialogue_balloon(
+			preload("res://interactables/dialogue/intro.dialogue"),
+			"intro"
+		)
+		await DialogueManager.dialogue_ended
+		balloon.queue_free() # avoid double showing this balloon bug
+		player.set_locked(false)
