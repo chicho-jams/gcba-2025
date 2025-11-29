09d98b423ab008b75901643a2cf2cfb0557ebb7d
Catania Gian Franco <catania.gian@gmail.com>
2025-11-28 21:13:50 -0300

    Updated

--- scenes/UI/main_menu.gd
+++ scenes/UI/main_menu.gd
@@ -1,14 +1,15 @@
 extends Control
 
-signal start_game()
 @onready var button_v_box: VBoxContainer = %ButtonVBox
+@onready var _main_menu_animation_player: AnimationPlayer = $MainMenuAnimationPlayer
+
+var _new_scene = preload("res://scenes/levels/level.tscn")
 
 func _ready() -> void:
 	focus_button()
 
 func _on_start_button_pressed() -> void:
-	start_game.emit()
-	hide()
+	_main_menu_animation_player.play("fade_in")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
@@ -22,3 +23,6 @@ func focus_button() -> void:
 		var button: Button = button_v_box.get_child(0)
 		if button is Button:
 			button.grab_focus()
+
+func _on_main_menu_animation_player_animation_finished(anim_name: StringName) -> void:
+	get_tree().change_scene_to_packed(_new_scene)
